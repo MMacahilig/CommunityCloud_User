af9ee4d6129066d0d88d0aba2e21d405653a01ed
MichaelPeterMacahilig11035060 <michael.p.macahilig@student.uts.edu.au>
2015-10-14 16:15:08 +1100

    added headers to receiveEvent

--- routes/cloud.js
+++ routes/cloud.js
@@ -53,8 +53,20 @@ router.post('/alert', function(req, res, next) {
 });
 
 router.post('/receiveEvent', function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-   var newEvent = new Event({
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+    var newEvent = new Event({
         alertType: req.body.AlertType,
         details: req.body.details,
         location: req.body.location,
