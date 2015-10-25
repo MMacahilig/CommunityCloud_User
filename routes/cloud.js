3cbcc6404430d119d3bb9b8895b0e177b231b1b2
MichaelPeterMacahilig11035060 <michael.p.macahilig@student.uts.edu.au>
2015-10-25 23:04:48 +1100

    changed find query

--- routes/cloud.js
+++ routes/cloud.js
@@ -195,7 +195,7 @@ router.get('/getEvents', function(req,res,next){
 router.get('/getuser', function(req,res,next){
     //console.log(req.query);
     var id = req.query._id;
-    User.find({userId: id}).lean().exec(function(err,docs){
+    User.find({_id: id}).lean().exec(function(err,docs){
         res.send(JSON.stringify(docs));
     });
     //res.send(JSON.stringify(req.query));
