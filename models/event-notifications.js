9a69ccbd4dc0f272d824218af1719b3f485c1eca
MichaelPeterMacahilig11035060 <michael.p.macahilig@student.uts.edu.au>
2015-10-28 13:40:01 +1100

    modified event notifications

--- models/event-notifications.js
+++ models/event-notifications.js
@@ -4,9 +4,10 @@ var Schema = mongoose.Schema;
 
 var eventNotificationSchema = new Schema({
     UserId: Schema.ObjectId,
-    createdBy: Schema.ObjectId,
-    dismissed: Boolean,
+    createdBy: String,
+    createdId: Schema.ObjectId,
     eventId: Schema.ObjectId,
+    dismissed: Boolean,
     created: {type: Date, Default: Date.now()}
 });
 
