8beb9af58cc6a4ce1a22df6ece3d918a08c78621
MichaelPeterMacahilig11035060 <michael.p.macahilig@student.uts.edu.au>
2015-11-06 01:01:37 +1100

    fixed route to get user events

--- routes/cloud.js
+++ routes/cloud.js
@@ -257,8 +257,7 @@ router.delete('/deleteAlerts', function(req, res, next) {
 
 router.get('/getuserevents', function(req,res,next){
 
-
-    EventNotification.find({UserId: req.query._id}).lean().exec(function(err,docs) {
+    EventNotification.find({UserId: req.query.id}).lean().exec(function(err,docs) {
         var alertString = [];
         var queryString = "[";
         var i = 0;
